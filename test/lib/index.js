d692d49dd2003ad4eb6a8fe911705e6c67888ca0
Pascal Laenen <pascal@ota-160.tc-online.local>
2016-02-26 14:20:33 +0000

    rewrite helper class to support versions

--- test/lib/index.js
+++ test/lib/index.js
@@ -1,76 +1,19 @@
 'use strict';
 
 import assert from 'assert';
-import helper from '../../lib';
-import payloadSchema from '../fixtures/validationSchema';
+import AwsHelper from './../../lib';
+import awsMock from 'aws-sdk-mock';
 
 describe('AWS Lambda helper', () => {
-  describe('validateWithSchema', () => {
-    it('should throw error for invalid data', (done) => {
-      const invalidData = {
-        body: {
-          docs: [{
-            param1: 'value1',
-            param2: ['123', '456'],
-            param3: 'standard'
-          }]
-        }
-      };
+  it('should get environment variable from context', (done) => {
+    const context = {
+      'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:447022989235:function:aws-canary-lambda:prod'
+    };
+    console.log(JSON.stringify(AwsHelper, null, 2));
+    let awsHelper = new AwsHelper(awsMock, context, {});
 
-      try {
-        let result = helper.validateWithSchema(invalidData, payloadSchema);
-        done(result);
-      } catch (error) {
-        assert.equal(error.message, 'Missing required property: param4');
-        done();
-      }
-    });
-
-    it('should return true for valid data', (done) => {
-      const validData = {
-        body: {
-          docs: [{
-            param1: 'value1',
-            param2: ['123', '456'],
-            param3: 'standard',
-            param4: 5
-          }]
-        }
-      };
-
-      let result = helper.validateWithSchema(validData, payloadSchema);
-      assert.equal(result, true);
-      done();
-    });
-  });
-
-  describe('getEnvironment', () => {
-    it('should get environment variable from context', (done) => {
-      const context = {
-        invokedFunctionArn: 'arn:123:abs:prod'
-      };
-
-      let env = helper.getEnvironment(context);
-      assert.equal(env, 'prod');
-      done();
-    });
-
-    it('should return null if invokedFunctionArn does not exist in context', (done) => {
-      let env = helper.getEnvironment({});
-
-      assert.equal(env, null);
-      done();
-    });
-
-    it('should return null if evn does not exist in invokedFunctionArn', (done) => {
-      const badContext = {
-        invokedFunctionArn: 'arn_123abs_prod:'
-      };
-
-      let env = helper.getEnvironment(badContext);
-
-      assert.equal(env, null);
-      done();
-    });
+    assert.equal(awsHelper.version, 'prod');
+    assert.equal(awsHelper.env, 'prod');
+    done();
   });
 });

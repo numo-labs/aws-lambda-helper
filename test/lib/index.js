8d7a7e0a9bc779ca9ef69909ee1b15b7ef2daddc
nelsonic <contact.nelsonic+github@gmail.com>
2016-02-27 19:10:33 +0000

    adds test for error thrown when invokedFunctionArn is invalid fixes #6

--- test/lib/index.js
+++ test/lib/index.js
@@ -6,43 +6,54 @@ var awsMock = require('aws-sdk-mock');
 
 describe('AWS Lambda helper', function () {
   describe('getEnvironment', function () {
-    // it('should get environment variable from context', (done) => {
-    //   const context = {
-    //     invokedFunctionArn: 'arn:123:abs:prod'
-    //   };
-    //
-    //   let env = helper.getEnvironment(context);
-    //   assert.equal(env, 'prod');
-    //   done();
-    // });
-    //
-    // it('should return null if invokedFunctionArn does not exist in context', (done) => {
-    //   let env = helper.getEnvironment({});
-    //
-    //   assert.equal(env, null);
-    //   done();
-    // });
-    //
-    // it('should return null if evn does not exist in invokedFunctionArn', (done) => {
-    //   const badContext = {
-    //     invokedFunctionArn: 'arn_123abs_prod:'
-    //   };
-    //
-    //   let env = helper.getEnvironment(badContext);
-    //
-    //   assert.equal(env, null);
-    //   done();
-    // });
+    it('should throw error when invokedFunctionArn is invalid', (done) => {
+      var badContext = {
+        invokedFunctionArn: 'arn:123:abs:prod'
+      };
+      try {
+        AwsHelper.getEnvironment(badContext);
+      } catch (e) {
+        var expected_err_msg = 'Error: Unexpected invokedFunctionArn format';
+        assert(e.toString().indexOf(expected_err_msg) > -1);
+        done();
+      }
+    });
 
-    it.only('should get environment variable from context', function (done) {
+    it('should return null if invokedFunctionArn is not set in context', (done) => {
+      var env = AwsHelper.getEnvironment({});
+      assert.equal(env, null);
+      done();
+    });
+
+    it('a valid ARN without environment should not set the env property', (done) => {
       var context = {
-        'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:447022989235:function:aws-canary-lambda:prod'
+        'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:123456789:function:mylambda'
+      };
+
+      var env = AwsHelper.getEnvironment(context);
+      console.log(env);
+      assert.equal(env, null);
+      done();
+    });
+
+    it('should get environment variable from context', function (done) {
+      var context = {
+        'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:123456789:function:aws-canary-lambda:prod'
       };
       var awsHelper = AwsHelper(awsMock, context, {});
-
-      assert.equal(awsHelper.version, 'prod');
       assert.equal(awsHelper.env, 'prod');
       done();
     });
+
+    // it('', function (done) {
+    //   var context = {
+    //     'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:447022989235:function:aws-canary-lambda:prod'
+    //   };
+    //   var awsHelper = AwsHelper(awsMock, context, {});
+    //
+    //   assert.equal(awsHelper.version, 'prod');
+    //   assert.equal(awsHelper.env, 'prod');
+    //   done();
+    // });
   });
 });

171c8a7a4349e42bca4d4abc928fc3c79799d1b6
Nikhila Ravi <nikkiravi92@gmail.com>
2016-03-03 16:06:45 +0000

    updates tests

--- test/lib/index.js
+++ test/lib/index.js
@@ -3,6 +3,10 @@
 var assert = require('assert');
 var AwsHelper = require('./../../lib/index');
 var awsMock = require('aws-sdk-mock');
+var mockContext = {
+  'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:123456789:function:aws-canary-lambda'
+};
+var aws = AwsHelper(mockContext);
 
 describe('AWS Lambda helper', function () {
   describe('getEnvironment', function () {
@@ -29,7 +33,6 @@ describe('AWS Lambda helper', function () {
       var context = {
         'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:123456789:function:mylambda'
       };
-
       var env = AwsHelper.getEnvironment(context);
       assert.equal(env, null);
       done();
@@ -39,52 +42,86 @@ describe('AWS Lambda helper', function () {
       var context = {
         'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:123456789:function:aws-canary-lambda:prod'
       };
-      var awsHelper = AwsHelper(awsMock, context, {});
-      assert.equal(awsHelper.env, 'prod');
-      done();
-    });
-
-    it('config should be set to an empty object if undefined', function (done) {
-      var context = {
-        'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:123456789:function:aws-canary-lambda:prod'
-      };
-      var awsHelper = AwsHelper(awsMock, context);
-      assert.deepEqual(awsHelper._config, {});
+      var aws = AwsHelper(context);
+      assert.equal(aws.contextParams.env, 'prod');
       done();
     });
   });
 
-  describe('AwsHelper.Lambda.invoke', function () {
+  describe('aws.Lambda.invoke', function () {
     it('should throw an error if the params.FunctionName is not set', function (done) {
       try {
-        AwsHelper.Lambda.invoke();
+        aws.Lambda.invoke();
       } catch (e) {
-        // console.log(e);
-        var expected_err_msg = 'Error: params.FunctionName is required';
-        assert(e.toString().indexOf(expected_err_msg) > -1);
+        var expected_err_msg = 'params.FunctionName is required';
+        assert(e.message.indexOf(expected_err_msg) > -1);
         done();
       }
     });
 
     it('should (Mock) invoke the function (using aws-sdk-mock)', function (done) {
-      var context = {
-        'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:123456789:function:aws-canary-lambda'
-      };
-      var awsMock = require('aws-sdk-mock');
       // set up the mock for lambda invocation using aws-sdk-mock:
       awsMock.mock('Lambda', 'invoke', 'totes worked');
       // instantiate the helper module:
-      AwsHelper(awsMock, context, {});
-      // assert.equal(awsHelper.env, '$LATEST'); // confirm correctly instantiated
-
       var params = {
         FunctionName: 'MyAmazingLambda',
         Payload: { 'hello': 'world' },
         Qualifier: ''
       };
-      AwsHelper.Lambda.invoke(params, function (err, data) {
+      aws.Lambda.invoke(params, function (err, data) {
         assert(err === null);
         assert(data === 'totes worked');
+        done();
+      });
+    });
+  });
+
+  describe('aws.SNS.publish', function () {
+    it('should throw an error if the params.TopicName is not set', function (done) {
+      try {
+        aws.Sns.publish();
+      } catch (e) {
+        var expected_err_msg = 'params.TopicName is required';
+        assert(e.message.indexOf(expected_err_msg) > -1);
+        done();
+      }
+    });
+    it('should (Mock) publish the message to the topic (using aws-sdk-mock)', function (done) {
+      awsMock.mock('SNS', 'publish', 'message published');
+      var params = {
+        Message: JSON.stringify({name: 'name'}),
+        TopicName: 'my-awesome-topic'
+      };
+      aws.Sns.publish(params, function (err, data) {
+        assert(err === null);
+        assert(data === 'message published');
+        done();
+      });
+    });
+  });
+
+  describe('aws.DynamoDB.putItem', function () {
+    it('should throw an error if the params.TableName is not set', function (done) {
+      try {
+        aws.DynamoDB.putItem();
+      } catch (e) {
+        var expected_err_msg = 'params.TableName is required';
+        assert(e.message.indexOf(expected_err_msg) > -1);
+        done();
+      }
+    });
+    it('should (Mock) publish the message to the topic (using aws-sdk-mock)', function (done) {
+      awsMock.mock('DynamoDB', 'putItem', 'item inserted');
+      var params = {
+        Item: {
+          key: { S: 'string' },
+          value: { S: 'string' }
+        },
+        TableName: 'my-awesome-table'
+      };
+      aws.DynamoDB.putItem(params, function (err, data) {
+        assert(err === null);
+        assert(data === 'item inserted');
         done();
       });
     });

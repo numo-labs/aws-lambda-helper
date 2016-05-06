74ad4cc0db8597da3f8d42f96b190085d0987c12
Tom Gallacher <tomgallacher23@gmail.com>
2016-05-06 12:34:55 +0100

    Added `Logger` function for JSON log messages

---
+++ test/lib/logger.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var assert = require('assert');
+var AwsHelper = require('./../../lib/index');
+var interceptStdout = require('intercept-stdout');
+
+describe('AwsHelper.Logger', function () {
+  it('should accept a parameter which is optional with no value', function (done) {
+    var context = {
+      'functionName': 'aws-canary-lambda',
+      'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:123456789:function:aws-canary-lambda:prod'
+    };
+    AwsHelper.init(context);
+    var logger = AwsHelper.Logger();
+    assert.equal(logger.fields.module, '');
+    done();
+  });
+
+  it('should accept a parameter which is optional with a value', function (done) {
+    var context = {
+      'functionName': 'aws-canary-lambda',
+      'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:123456789:function:aws-canary-lambda:prod'
+    };
+    AwsHelper.init(context);
+    var logger = AwsHelper.Logger('hello');
+    assert.equal(logger.fields.module, 'hello');
+    done();
+  });
+
+  describe('AwsHelper.Logger#info', function () {
+    it('should log a json message', function (done) {
+      var context = {
+        'functionName': 'aws-canary-lambda',
+        'invokedFunctionArn': 'arn:aws:lambda:eu-west-1:123456789:function:aws-canary-lambda:prod'
+      };
+      AwsHelper.init(context);
+      var logger = AwsHelper.Logger();
+      var stdout = '';
+      var unhook = interceptStdout(function (text) {
+        stdout += text;
+      });
+      logger.info('hello in stdout');
+      setImmediate(function () {
+        unhook();
+        var json = JSON.parse(stdout);
+        assert.equal(json.msg, 'hello in stdout');
+        done();
+      });
+    });
+  });
+});

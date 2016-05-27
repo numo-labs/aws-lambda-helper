86658e28a7cdc310f435f6297a1470a469ce4bd3
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-27 16:22:13 +0100

    remove http_request from lib/index.js (simplify things!)

--- test/lib/s3saveget.test.js
+++ test/lib/s3saveget.test.js
@@ -1,7 +1,6 @@
 require('env2')('.env');
-// console.log(process.env);
 var assert = require('assert');
-// var s3 = require('./../../lib/s3saveget.js');
+var request = require('wreck');
 var AwsHelper = require('./../../lib/index');
 
 var PARAMS = {
@@ -10,7 +9,10 @@ var PARAMS = {
   name: 'My Amazing Hotel',
   connectionId: 'test1234',
   userId: 'TESTUSERID',
-  items: [{id: 12345, title: 'my amazing hotel'}]
+  items: [
+    {id: 12345, title: 'my amazing hotel', url: 'test/test/12345'},
+    {id: 45678, title: 'my lovely resort', url: 'test/test/45678'}
+  ]
 };
 
 describe('saveRecordToS3', function () {
@@ -18,20 +20,14 @@ describe('saveRecordToS3', function () {
     AwsHelper.init({
       invokedFunctionArn: 'arn:aws:lambda:eu-west-1:123456789:function:mylambda:ci'
     });
-    var params = JSON.parse(JSON.stringify(PARAMS));
-    params.items[0].url = params.userId + '/' + params.connectionId + '/' +
-      params.bucketId + '/' + params.items[0].id;
-    AwsHelper.saveRecordToS3(params, function (err, data) {
+    AwsHelper.saveRecordToS3(PARAMS, function (err, data) {
       assert(!err, 'No Errror Saving the Record');
-      var url = data.Location.replace('https://', '');
-      console.log(url);
-      var httpParams = {
-        host: url.split('/')[0],
-        path: url.split('.com')[1]
-      };
-      AwsHelper.httpRequest(httpParams, function (err, data) {
+      // console.log('S3 >>', err, data);
+      request.get(data.Location.toString(), function (err, data, payload) {
+        // console.log(err,  payload.toString());
+        var json = JSON.parse(payload.toString());
         assert(!err, 'No Errror retrieving the Record');
-        assert.equal(data.id, params.items[0].id, 'record Successfully saved to S3');
+        assert.equal(json.id, PARAMS.items[1].id, 'record Successfully saved to S3');
         done();
       });
     });

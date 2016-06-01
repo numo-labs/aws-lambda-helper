534a921abadcc765d1e6d3256103c10c6835090b
Leonard Martin <leonard.martin@gmail.com>
2016-06-01 17:18:14 +0100

    Remove items with n url prop when saving to S3 on publish
    We want to be able to send items back into the results pipe to the client without also necessarily saving them to s3 as well. In particular, it does not make sense to save items to s3 which do not url.
    
    As such, we remove items from the items array which have no url property before saving to s3.
    
    Also slightly refactor the tests for this method to more explicitly define the calls to downstream AWS services by stubbing the AWS SDK.

--- test/lib/pushResultToClient.test.js
+++ test/lib/pushResultToClient.test.js
@@ -1,7 +1,9 @@
 // this test needs to run *FIRST* for some reason ... any help figuring out why much appreciated.
 require('env2')('.env');
-var assert = require('assert');
-var AwsHelper = require('./../../lib/index');
+const assert = require('assert');
+const simple = require('simple-mock');
+const AWS = require('aws-sdk');
+const AwsHelper = require('./../../lib/index');
 
 describe('pushResultToClient', function () {
   before('Connect to WebSocket Server', function (done) {
@@ -10,26 +12,108 @@ describe('pushResultToClient', function () {
     });
     done();
   });
+  beforeEach(() => {
+    simple.mock(AWS.S3.prototype, 'upload').callbackWith();
+    simple.mock(AWS.SNS.prototype, 'publish').callbackWith();
+  });
+  afterEach(() => {
+    simple.restore();
+  });
 
-  it('Send result to Client *AND* Save to S3', function (done) {
+  it('publishes results to SNS', (done) => {
     var params = {
       id: 'dummyConnectionId', // the session id from WebSocket Server
       searchId: 'ABC',
       userId: 'TESTUSERID',
-      items: [{
-        id: 123, hello: 'world', title: 'amazing holiday',
-        url: 'userId/connectionId/bucketId/123'
-      }]
+      items: [
+        {
+          id: 123, hello: 'world', title: 'amazing holiday',
+          url: 'userId/connectionId/bucketId/123'
+        },
+        {
+          id: 456, hello: 'world', title: 'not amazing holiday',
+          url: 'userId/connectionId/bucketId/456'
+        }
+      ]
     };
-    AwsHelper.pushResultToClient(params, function (err, res) {
-      // assert(!err);
-      console.log(err);
-      assert.equal(res.Key, 'ci/' + params.items[0].url + '.json');
+    const expected = JSON.stringify({
+      default: JSON.stringify(params)
+    });
+    AwsHelper.pushResultToClient(params, function (err) {
+      assert(!err);
+      assert.equal(AWS.SNS.prototype.publish.callCount, 1);
+      assert.equal(AWS.SNS.prototype.publish.calls[0].args[0].Message, expected);
       done();
     });
   });
 
-  it('Send result to Client *AND* Save to S3 (no SNS Topic)', function (done) {
+  it('saves items to S3', function (done) {
+    var params = {
+      id: 'dummyConnectionId', // the session id from WebSocket Server
+      searchId: 'ABC',
+      userId: 'TESTUSERID',
+      items: [
+        {
+          id: 123, hello: 'world', title: 'amazing holiday',
+          url: 'userId/connectionId/bucketId/123'
+        },
+        {
+          id: 456, hello: 'world', title: 'not amazing holiday',
+          url: 'userId/connectionId/bucketId/456'
+        }
+      ]
+    };
+    AwsHelper.pushResultToClient(params, function (err) {
+      assert(!err);
+      assert.equal(AWS.S3.prototype.upload.callCount, 2);
+      assert.equal(AWS.S3.prototype.upload.calls[0].args[0].Body, JSON.stringify(params.items[0]));
+      assert.equal(AWS.S3.prototype.upload.calls[1].args[0].Body, JSON.stringify(params.items[1]));
+      done();
+    });
+  });
+
+  it('does not save items without a url to S3', function (done) {
+    var params = {
+      id: 'dummyConnectionId', // the session id from WebSocket Server
+      searchId: 'ABC',
+      userId: 'TESTUSERID',
+      items: [
+        {
+          id: 123, hello: 'world', title: 'amazing holiday',
+          url: 'userId/connectionId/bucketId/123'
+        },
+        {
+          id: 456, hello: 'world', title: 'not amazing holiday'
+        }
+      ]
+    };
+    AwsHelper.pushResultToClient(params, function (err, res) {
+      assert(!err);
+      assert.equal(AWS.S3.prototype.upload.callCount, 1);
+      assert.equal(AWS.S3.prototype.upload.lastCall.args[0].Body, JSON.stringify(params.items[0]));
+      done();
+    });
+  });
+
+  it('skips saving to s3 if no items have url properties', function (done) {
+    var params = {
+      id: 'dummyConnectionId', // the session id from WebSocket Server
+      searchId: 'ABC',
+      userId: 'TESTUSERID',
+      items: [
+        {
+          id: 456, hello: 'world', title: 'not amazing holiday'
+        }
+      ]
+    };
+    AwsHelper.pushResultToClient(params, function (err, res) {
+      assert(!err);
+      assert.equal(AWS.S3.prototype.upload.callCount, 0);
+      done();
+    });
+  });
+
+  it('throws an error if SNS topic is not defined', function (done) {
     delete process.env.SEARCH_RESULT_TOPIC;
     var params = {
       id: 'dummyConnectionId', // the session id from WebSocket Server
